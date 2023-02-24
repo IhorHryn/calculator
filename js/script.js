aac66399008c4565378d5aa4c640bb4190dbeaa6
Ihor <ihorhryn2003@gmail.com>
2023-02-24 17:40:42 +0200

    calculator

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+const firstNumber = Number(prompt("Введите первое число:"));
+
+const secondNumber = Number(prompt("Введите второе число:"));
+
+const sum = firstNumber + secondNumber;
+const diff = firstNumber - secondNumber;
+const mult = firstNumber * secondNumber;
+const div = firstNumber / secondNumber;
+
+console.log(`Calculations are finished!\nSum:
+${firstNumber} + ${secondNumber} = ${sum}\nDiff:
+${firstNumber} - ${secondNumber} = ${diff}\nMult:
+${firstNumber} * ${secondNumber} = ${mult}\nDiv:
+${firstNumber} / ${secondNumber} = ${div}`);
